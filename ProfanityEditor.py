0264f97415071ec03cd605fd39ecb1d9cee68e26
spotapov <sergiy.potapov@gmail.com>
2015-07-31 19:05:38 +0300

    +urllib

--- ProfanityEditor.py
+++ ProfanityEditor.py
@@ -1,7 +1,14 @@
 __author__ = 'spotapov'
+import urllib
 def read_test():
-    file = open("C:/Users/SPotapov/PycharmProjects/untitled1/check_profanity.txt")
-    text = file.read()
+    open_file = open("C:/Users/SPotapov/PycharmProjects/untitled1/check_profanity.txt")
+    text = open_file.read()
     print(text)
-    file.close()
+    open_file.close()
+    check_profanity(text)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    out = connection.read()
+    print(out)
 read_test()
